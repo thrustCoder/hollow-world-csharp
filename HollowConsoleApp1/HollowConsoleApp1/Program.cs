062c30ab7f4e588260596993bb94789119a7919f
Rajarshi Singh <rajarshi129@gmail.com>
2021-07-01 14:19:04 -0700

    Simple console app with linked list example.

---
+++ HollowConsoleApp1/HollowConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HollowConsoleApp1.Service;
+
+namespace HollowConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hollow World");
+
+			LinkedList.PrepFindLength();
+		}
+	}
+}
---
+++ HollowConsoleApp1/HollowConsoleApp1/Service/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HollowConsoleApp1.Model;
+
+namespace HollowConsoleApp1.Service
+{
+	class LinkedList
+	{
+		public static void PrepFindLength() 
+		{
+			LinkedListNodeCustom l11 = new LinkedListNodeCustom(1);
+			LinkedListNodeCustom l12 = new LinkedListNodeCustom(2);
+			LinkedListNodeCustom l13 = new LinkedListNodeCustom(3);
+			LinkedListNodeCustom l14 = new LinkedListNodeCustom(4);
+
+			l11.Next = l12;
+			l12.Next = l13;
+			l13.Next = l14;
+
+			Console.WriteLine("Length of linked list: " + Length(l11).ToString());
+		}
+
+		private static int Length(LinkedListNodeCustom head)
+		{
+			int len = 0;
+			LinkedListNodeCustom current = head;
+
+			while (current != null)
+			{
+				len++;
+				current = current.Next;
+			}
+
+			return len;
+		}
+	}
+}
